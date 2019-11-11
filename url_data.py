76b96e17ad08da55024efd234c3e55d6129b3c47
15211687881 <1094630886@qq.com>
2019-11-11 22:06:39 +0800

    使用到的URL

---
+++ url_data.py
@@ -0,0 +1,10 @@
+# 登录使用到的URL
+login_url = 'http://cas.swust.edu.cn/authserver/login?service=http://my.swust.edu.cn/mht_shall/a/service/serviceFrontManage/cas'  # 登录一站式服务大厅
+code_url = 'http://cas.swust.edu.cn/authserver/captcha'  # 获取验证码
+key_url = 'http://cas.swust.edu.cn/authserver/getKey'  # 获取公钥
+login_main_url = 'http://cas.swust.edu.cn/authserver/login?service=https://matrix.dean.swust.edu.cn/acadmicManager/index.cfm?event=studentPortal:DEFAULT_EVENT'  # 登录理论课教务处系统
+get_main_url = 'https://matrix.dean.swust.edu.cn/acadmicManager/index.cfm?event=studentPortal:courseTable'  # 跳转到理论课课表
+test_id_url = 'http://202.115.175.177/swust/'  # 获取登录cookie
+login_test_url = 'http://202.115.175.177'  # 登录实验系统
+test_index_url = 'http://202.115.175.177/StuExpbook/login.jsp'  # 跳转到实验系统主页
+get_test_url = 'http://202.115.175.177/StuExpbook/book/bookResult.jsp'  # 跳转到实验课课表
